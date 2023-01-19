55179e93c0ca78e5d3bafe76e6ac66bd147914b6
emir <emirkorkut35@gmail.com>
2023-01-20 00:45:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+var port=process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+
+ // res.send('Merhaba Express');
+  //res.send('<h1>Merhaba Express</h1>');
+  res.sendFile( __dirname + "/" + "earth.html");
+
+});
+
+app.post('/', function (req, res) {
+ // res.send('Merhaba Express');
+  res.sendFile( __dirname + "/" + "earth.html");
+});
+
+app.listen(port, function () {
+  console.log('Sunucu çalışıyor...');
+});
