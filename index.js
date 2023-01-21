b9e467bc7a31b9a6beaf392709d81057cd97ce7d
korkutemir <55914306+korkutemir@users.noreply.github.com>
2023-01-21 13:39:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-require("dotenv").config();
+//require("dotenv").config();
 const express = require('express');
 const app = express();
 var port=process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
 
- // res.send('Merhaba Express');
+  res.send('Merhaba Express');
   //res.send('<h1>Merhaba Express</h1>');
-  res.sendFile( __dirname + "/" + "earth.html");
+ // res.sendFile( __dirname + "/" + "earth.html");
 
 });
 
