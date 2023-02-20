ce18f365b306c44f7a942b04e2a68adeb0971fb1
emir <emirkorkut35@gmail.com>
2023-02-20 17:55:14 +0300

    korkut

--- index.js
+++ index.js
@@ -36,15 +36,9 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,ALL');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-});
+}); 
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'http://localhost:8080');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-  });
+
 
 app.use((req, res, next) => {
     res.setHeader('X-Content-Type-Options', 'nosniff');
