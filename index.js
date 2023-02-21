84e870fa147ed07bfe019a29050deac2267c991d
emir <emirkorkut35@gmail.com>
2023-02-21 14:18:24 +0300

    korkut

--- index.js
+++ index.js
@@ -526,6 +526,182 @@ app.get('/notification', function(req, res) {
     res.send(message)
   });
 
+
+  app.get('/megagen-uuid/:id',function(req,res,next) {
+    // res.sendFile( __dirname + "/" + "pwa.html");
+    // res.download('eko.pdf');
+ 
+    console.log(req.query);
+ 
+    var dizin_1=[];
+    var dizin_2=[];
+    var dizin_3=[];
+ 
+    console.log(Object.keys(req.query).length);
+ 
+    if(Object.keys(req.query).length !==0) {
+ 
+     
+ 
+     const MongoClient = require('mongodb').MongoClient;
+   //  const URL = 'mongodb://localhost:27017';
+     const URL = 'mongodb+srv://megagen:megagen07@cluster0.p6cbt1r.mongodb.net/?authSource=admin';
+ 
+     MongoClient.connect(URL,(err,client)=> {
+      if(err) throw err;
+      const db = client.db('megagen');
+      db.collection('deviceid').find(req.query.deviceid, { projection: { _id: 0 } }).toArray(async (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    
+      await device();
+     
+      });
+     });
+     
+    function  device() {
+     
+       
+         
+         MongoClient.connect(URL, (err, client) => {
+           if (err) throw err;
+         
+           const db = client.db('megagen');
+           let sorgu_pro = {membership:"pro"};
+           let sorgu_premium = {membership:"premium"};
+           let sorgu_free = {membership:"free"};
+           var sorgu3=req.query;
+           let sorgu_2={name:"Section"}
+           db.collection('anyridge').find(sorgu_pro, { projection: { _id: 0 } }).toArray((err, result) => {
+             if (err) throw err;
+             console.log(result);
+         
+            // res.send(result);
+         
+            // client.close();
+ 
+            dizin_1.push(result);
+           });
+ 
+           db.collection('anyridge').find(sorgu_premium, { projection: { _id: 0 } }).toArray((err, result) => {
+             if (err) throw err;
+             console.log(result);
+         
+            // res.send(result);
+         
+            // client.close();
+ 
+            dizin_1.push(result);
+           });
+ 
+           db.collection('anyridge').find(sorgu_free, { projection: { _id: 0 } }).toArray((err, result) => {
+             if (err) throw err;
+             console.log(result);
+         
+           //  res.send(result);
+         
+            // client.close();
+ 
+            dizin_1.push(result);
+ 
+           // res.send(dizin_1);
+           });
+ 
+           db.collection('anyone').find(sorgu_pro, { projection: { _id: 0 } }).toArray((err, result) => {
+             if (err) throw err;
+             console.log(result);
+         
+           //  res.send(result);
+         
+            // client.close();
+ 
+            dizin_2.push(result);
+ 
+          //  res.send(dizin_1);
+           });
+ 
+           db.collection('anyone').find(sorgu_premium, { projection: { _id: 0 } }).toArray((err, result) => {
+             if (err) throw err;
+             console.log(result);
+         
+           //  res.send(result);
+         
+            // client.close();
+ 
+            dizin_2.push(result);
+ 
+          //  res.send(dizin_1);
+           });
+ 
+           db.collection('anyone').find(sorgu_free, { projection: { _id: 0 } }).toArray((err, result) => {
+             if (err) throw err;
+             console.log(result);
+         
+           //  res.send(result);
+         
+            // client.close();
+ 
+            dizin_2.push(result);
+ 
+          //  res.send({dizin_1,dizin_2});
+           });
+ 
+           db.collection('st').find(sorgu_pro, { projection: { _id: 0 } }).toArray((err, result) => {
+             if (err) throw err;
+             console.log(result);
+         
+           //  res.send(result);
+         
+            // client.close();
+ 
+            dizin_3.push(result);
+ 
+          //  res.send({dizin_1,dizin_2});
+           });
+ 
+           db.collection('st').find(sorgu_premium, { projection: { _id: 0 } }).toArray((err, result) => {
+             if (err) throw err;
+             console.log(result);
+         
+           //  res.send(result);
+         
+            // client.close();
+ 
+            dizin_3.push(result);
+ 
+          //  res.send({dizin_1,dizin_2});
+           });
+ 
+           db.collection('st').find(sorgu_free, { projection: { _id: 0 } }).toArray(async (err, result) => {
+             if (err) throw err;
+             console.log(result);
+         
+           //  res.send(result);
+         
+            // client.close();
+ 
+           await dizin_3.push(result);
+ 
+            res.send({dizin_1,dizin_2,dizin_3});
+           });
+ 
+         
+         
+         }); 
+ 
+     }
+ 
+  
+ 
+
+ 
+   
+    }
+ 
+   
+ }); 
+
+
 var httpServer = http.createServer(app);
 //var httpsServer = https.createServer(credentials, app); 
 
