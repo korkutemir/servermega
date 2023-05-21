efe83a71b14fd88ecdf1ba9a6cb82ad96c13d4b6
korkutemir <55914306+korkutemir@users.noreply.github.com>
2023-05-22 01:07:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,10 @@ app.use((req, res, next) => {
   });
   
 
-
+app.use((req, res, next) => {
+    res.status(404).send(
+        "<h1>Page not found on the server</h1>")
+});
 
 
 app.get('/',function(req,res,next) {
